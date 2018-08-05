dbc8b07905879c2147996033d25b2280f2a9cd77
MisfitSphinx <almagroag@outlook.com>
2018-08-05 15:50:11 +0200

    On branch master  Changes to be committed: 	modified:   rango/__pycache__/views.cpython-37.pyc 	modified:   rango/views.py 	modified:   tango_with_django_project/__pycache__/settings.cpython-37.pyc 	modified:   tango_with_django_project/settings.py 	new file:   templates/rango/index.html

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_PATH],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,6 +68,9 @@ TEMPLATES = [
         },
     },
 ]
+
+
+
 
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
